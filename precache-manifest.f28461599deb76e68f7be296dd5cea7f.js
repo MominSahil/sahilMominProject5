1bbb3f5b484b81084a32069e1c95bd2b95e773d4
MominSahil <67797239+MominSahil@users.noreply.github.com>
2020-09-30 22:22:11 -0400

    Updates

---
+++ precache-manifest.f28461599deb76e68f7be296dd5cea7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df426872739491e7fa382867873b210c",
+    "url": "/sahilMominProject5/index.html"
+  },
+  {
+    "revision": "9a8648d91d4b78d0bc67",
+    "url": "/sahilMominProject5/static/css/main.4ba143d3.chunk.css"
+  },
+  {
+    "revision": "cbfa98ea965e8f3f6739",
+    "url": "/sahilMominProject5/static/js/2.339acfd5.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/sahilMominProject5/static/js/2.339acfd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8648d91d4b78d0bc67",
+    "url": "/sahilMominProject5/static/js/main.ef0d50f7.chunk.js"
+  },
+  {
+    "revision": "1cdc477420944aebceb2",
+    "url": "/sahilMominProject5/static/js/runtime-main.63f028ed.js"
+  }
+]);
